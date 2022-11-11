ee8fc44e7a7c24932e2140902079f585c3ffc054
Chris Cole <christopher.a.cole@gmail.com>
2022-11-10 16:46:18 -0800

    add notation

--- cleanUp.js
+++ cleanUp.js
@@ -13,6 +13,7 @@ function nameQuestion(name) {
   console.log("Hello, what is your name?")
   console.log("My name is", name)
 }
+// curly on next line
 
 nameQuestion("Chris Cole")
 // added name to answer question
@@ -20,8 +21,9 @@ nameQuestion("Chris Cole")
 
 // EX 2:
 function addThreeNums(first, second, third) {
-var sum = first + second + third
-console.log(sum);
+  var sum = first + second + third
+  console.log(sum);
+  // indented 23 and 24
 }
 // removed whitespace on line 25
 addThreeNums(1, 2, 3);
@@ -29,13 +31,14 @@ addThreeNums(4, 2, 7);
 
 
 // EX 3:
-function makeFreshPesto(){
+function makeFreshPesto() {
   // typed out rest of function keyword
   console.log("Buy ingredients: basil, parmesan, romano, olive oil, pine nuts, garlic, salt, pepper");
   console.log("Pulse basil and pine nuts");
   console.log("Add garlic and cheeses");
   console.log("Slowly pour in oil");
-  console.log("Season");    }
+  console.log("Season");    
+}
 
 makeFreshPesto();
 
@@ -43,9 +46,10 @@ makeFreshPesto();
 //  EX 4:
 function average(num1, num2) {
   // backspace curly brace to 42
-var sum = num1 + num2;
-    var avg = sum / 2;
-
+  var sum = num1 + num2;
+  var avg = sum / 2;
+// indentation on variable declarations
   console.log(`the average is: ${avg}.`)
   // last period was outside backtic
-  }
+}
+// indentation on curly
